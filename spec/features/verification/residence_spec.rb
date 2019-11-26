422d38b301265edeca159b31fbeeaf543180d21b
Julian Herrero <microweb10@gmail.com>
2019-11-26 11:25:26 +0700

    Allow residence verification by zipcode
    Once the new code has been deployed to production and DB migrations
    have been run, a rake task needs to be executed in order to import the
    zipcodes from the CSV file to the DB
    
    `bin/rake db:zipcodes RAILS_ENV=production`
    
    After that, any user will be verified by just introducing a valid
    zipcode.
    
    For the error message when introducing a zipcode that is not in the
    list, the default error message shown will be this one:
    https://github.com/StemvanGroningen/consul/blob/master/config/locales/nl/verification.yml#L60
    
    It can be customized by adding the file:
    `config/locales/custom/nl/verification.yml`
    with the content
    
    ```
    nl:
      verification:
        residence:
          new:
            error_not_allowed_postal_code: "CUSTOM ERROR MESSAGE"
    ```

--- spec/features/verification/residence_spec.rb
+++ spec/features/verification/residence_spec.rb
@@ -2,23 +2,8 @@ require "rails_helper"
 
 describe "Residence" do
 
-  before { create(:geozone) }
-
-  scenario "Verify resident" do
-    user = create(:user)
-    login_as(user)
-
-    visit account_path
-    click_link "Verify my account"
-
-    fill_in "residence_document_number", with: "12345678Z"
-    select "DNI", from: "residence_document_type"
-    select_date "31-December-1980", from: "residence_date_of_birth"
-    fill_in "residence_postal_code", with: "28013"
-    check "residence_terms_of_service"
-    click_button "Verify residence"
-
-    expect(page).to have_content "Residence verified"
+  background do
+    Zipcode.create!(code: "9713BH")
   end
 
   scenario "Residence form use min age to participate" do
@@ -49,12 +34,12 @@ describe "Residence" do
     fill_in "residence_document_number", with: "12345678Z"
     select "DNI", from: "residence_document_type"
     select_date "31-December-1980", from: "residence_date_of_birth"
-    fill_in "residence_postal_code", with: "28013"
+    fill_in "residence_postal_code", with: "9713BH"
     check "residence_terms_of_service"
 
     click_button "Verify residence"
 
-    expect(page).to have_content "Residence verified"
+    expect(page).to have_content "Account verified"
 
     expect(vote.reload.voter).to eq(new_user)
     expect(erased_user.reload.document_number).to be_blank
@@ -71,74 +56,5 @@ describe "Residence" do
     click_button "Verify residence"
 
     expect(page).to have_content(/\d errors? prevented the verification of your residence/)
-  end
-
-  scenario "Error on postal code not in census" do
-    user = create(:user)
-    login_as(user)
-
-    visit account_path
-    click_link "Verify my account"
-
-    fill_in "residence_document_number", with: "12345678Z"
-    select "DNI", from: "residence_document_type"
-    select "1997", from: "residence_date_of_birth_1i"
-    select "January", from: "residence_date_of_birth_2i"
-    select "1", from: "residence_date_of_birth_3i"
-    fill_in "residence_postal_code", with: "12345"
-    check "residence_terms_of_service"
-
-    click_button "Verify residence"
-
-    expect(page).to have_content "In order to be verified, you must be registered"
-  end
-
-  scenario "Error on census" do
-    user = create(:user)
-    login_as(user)
-
-    visit account_path
-    click_link "Verify my account"
-
-    fill_in "residence_document_number", with: "12345678Z"
-    select "DNI", from: "residence_document_type"
-    select "1997", from: "residence_date_of_birth_1i"
-    select "January", from: "residence_date_of_birth_2i"
-    select "1", from: "residence_date_of_birth_3i"
-    fill_in "residence_postal_code", with: "28013"
-    check "residence_terms_of_service"
-
-    click_button "Verify residence"
-
-    expect(page).to have_content "The Census was unable to verify your information"
-  end
-
-  scenario "5 tries allowed" do
-    user = create(:user)
-    login_as(user)
-
-    visit account_path
-    click_link "Verify my account"
-
-    5.times do
-      fill_in "residence_document_number", with: "12345678Z"
-      select "DNI", from: "residence_document_type"
-      select "1997", from: "residence_date_of_birth_1i"
-      select "January", from: "residence_date_of_birth_2i"
-      select "1", from: "residence_date_of_birth_3i"
-      fill_in "residence_postal_code", with: "28013"
-      check "residence_terms_of_service"
-
-      click_button "Verify residence"
-      expect(page).to have_content "The Census was unable to verify your information"
-    end
-
-    click_button "Verify residence"
-    expect(page).to have_content "You have reached the maximum number of attempts. Please try again later."
-    expect(page).to have_current_path(account_path)
-
-    visit new_residence_path
-    expect(page).to have_content "You have reached the maximum number of attempts. Please try again later."
-    expect(page).to have_current_path(account_path)
   end
 end
